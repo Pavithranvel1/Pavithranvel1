832deaf9147955dbd779003991fd4d0510e46b34
pavithranrs <psoundararajan@kapitus.com>
2022-01-19 17:07:57 +0530

    slug change 2

--- pages/product-service/[slug].js
+++ pages/product-service/[slug].js
@@ -19,151 +19,24 @@ const Productservices = (props) => {
 
   const [isOpen, setOpen] = useState(false);
   const [back, setBack] = useState(true)
-  const lottieRef = useRef(null)
-  const lottieRef1 = useRef(null)
-  const lottieRef2 = useRef(null)
-  const [lottieimage, setLottieImage] = useState(lottieRef)
-  const [opacity, setOpacity] = useState(1);
-  const [snapPoint, setSnapPoint] = useState(0);
-  let [initial, setInitial] = useState(0);
-  const [value, setValue] = useState(0);
-
-  let easing = [0.175, 0.85, 0.42, 0.96];
-  const transition = { duration: 0.5, ease: easing, type: "spring", damping: 10, mass: 0.75, stiffness: 100 };
-  const textVariants = {
-    initial: { y: 150, opacity: 0, transition: { delay: .3, ...transition }},
-    animate: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 0.5,
-        ease: easing
-      }
-    },
-    exit: { y: 150, opacity: 0, transition: { delay:.5, duration: 1, ease: easing } }
-  };
-
-  const theme = useTheme();
-  const router = useRouter();
-  const [ref, inView ] = useInView({threshold: 0});
-  //const { ref, inView, entry } = useInView();
-  const [ref1, inView1 ] = useInView();
-  const animation = useAnimation();
-  const animation1 = useAnimation();
-  const productname = Object.keys(router.query).length >=1 ? router.query.slug.replaceAll('-', ' ') : ''
-
-  const a11yProps = (index) => {
-    return {
-      id: `full-width-tab-${index}`,
-      'aria-controls': `full-width-tabpanel-${index}`,
-    };
-  }
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const closeModal = (event) => {
-    setBack(false)
-    setTimeout(function(){
-      router.push("/products-offer")
-    }, 1000)
-  }
-
-  const handleClick = (event) => {
-    
-    let id = event.target.id.replace(/\D/g, "")
-    if(id==0){
-      setLottieImage(lottieRef)
-      router.push(`${sanitizeUrl()}#requirement`)
-    }
-    else if(id==1){
-      setLottieImage(lottieRef1)
-      router.push(`${sanitizeUrl()}#how-to-apply`)
-    }
-    else {
-      setLottieImage(lottieRef2)
-      router.push(`${sanitizeUrl()}#who-is-this-for`)
-    }
-  }
-
-  const sanitizeUrl = () => {
-    return router.asPath.replace('#requirement','').replace('#how-to-apply','').replace('#who-is-this-for','')
-  }
-
-  const handleChangeIndex = (index) => {
-    setValue(index)
-    if(index==0) {
-      setLottieImage(lottieRef)
-      router.push(`${sanitizeUrl()}#requirement`)
-    }
-    else if(index==1) {
-      setLottieImage(lottieRef1)
-      router.push(`${sanitizeUrl()}#how-to-apply`)
-    }
-    else {
-      setLottieImage(lottieRef2)
-      router.push(`${sanitizeUrl()}#who-is-this-for`)
-    }
-  };
-
-  const shortForm = (event) => {
-    setTimeout(function(){
-      setBack(false)
-      setOpen(true)
-      setInitial(0)
-      console.log(router.asPath)
-      router.push(`${router.asPath}#short-form`)
-    }, 700)
-  }
-
-  const onDrag = (event, info) => {
-
-    if (info.offset.y > 500) {
-      setTimeout(function(){
-        setBack(false)
-        router.push("/products-offer")
-      }, 1000)
-    }
-  }
-
-  const onDragUp = (event, info) => {
-    if (Math.abs(info.offset.y) > 250) {
-      setBack(false)
-      setOpen(true)
-      setInitial(0)
-      //router.push(`${router.query.slug}#short-form`)
-      router.push(`${router.asPath}#short-form`)
-    }
-  }
-
-  const close = () => {
-    setBack(true)
-    setOpen(false);
-  }
-
-  const closeSheet = () => {
-    setBack(true)
-    setOpen(false)
-    setInitial(0)
-  }
+  
 
   return (
     <>
     <AnimatePresence>
     {back && (
-      <div className="inset-0 pointer-events-auto" style={{opacity: opacity}}>
-      <motion.div className="fixed" initial="initial" animate="animate" exit="exit" drag="y" dragConstraints={{ top: 0, bottom:0 }} onDrag={onDrag} variants={textVariants}>
-        <motion.div className="grid fixed z-50 cursor-pointer text-white place-items-center w-full h-12 bg-kapitus text-lg capitalize" onClick={closeModal} initial={{ y: -350, opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: .5, ...transition }}}><div className="absolute left-0 pl-6"><FaChevronLeft size="20" /></div>{productname}
+      <div className="inset-0 pointer-events-auto">
+      <motion.div className="fixed" initial="initial" animate="animate" exit="exit" drag="y" dragConstraints={{ top: 0, bottom:0 }}>
+        <motion.div className="grid fixed z-50 cursor-pointer text-white place-items-center w-full h-12 bg-kapitus text-lg capitalize" initial={{ y: -350, opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: .5 }}}><div className="absolute left-0 pl-6"><FaChevronLeft size="20" /></div>saf
         </motion.div>
 
  
       <div className="pb-10 overflow-y-auto content" style={{ height: '500px'}}>
-      <motion.div ref={ref} className="px-7 py-2">
+      <motion.div className="px-7 py-2">
         <span className="text-kapitus font-bold text-3xl">Why Consider A Business Loan?</span>
         <p className="text-xl">Business loans are one of the most versatile forms of business financing available to owners on the market today.  They are available in a large range of sizes, come with an array of payment options and there is no limit on the way you can use the funds for your business. Whether you’re looking to grow, maintain daily operations, or build yourself a cash flow safety net to manage the unexpected, Kapitus can help you build the right loan product for your unique business needs.</p>
       </motion.div>
-      <motion.div ref={ref1} className="px-7 py-2">
+      <motion.div  className="px-7 py-2">
         <span className="text-kapitus font-bold text-3xl">Business Flexibility</span>
         <p className="text-xl">Kapitus business loans can be used for any business purpose, unlike business loans provided by traditional lenders and the SBA which often have restrictions on use associated with competitive rates and a variety of terms and payment plans available, you have the ability to build out a loan product that is created specifically for your business needs.</p>
       </motion.div>
@@ -185,10 +58,10 @@ const Productservices = (props) => {
       </div>
       </div>
         </motion.div>
-        <motion.nav className="fixed bottom-0 inset-x-0 z-10 w-full transition-colors bg-kapitus" drag="y" dragConstraints={{top: 0, bottom:0 }} onDrag={onDragUp}>
+        <motion.nav className="fixed bottom-0 inset-x-0 z-10 w-full transition-colors bg-kapitus" drag="y" dragConstraints={{top: 0, bottom:0 }}>
           <div className="flex items-center m-auto px-7 py-3">
             <div className="text-white text-base">Own a Small Business?</div>
-            <motion.div className="inline-flex text-kapitus items-center bg-white text-blue-700 font-semibold py-2 px-6 pb-3 rounded ml-auto" onClick={shortForm}>APPLY NOW</motion.div>
+            <motion.div className="inline-flex text-kapitus items-center bg-white text-blue-700 font-semibold py-2 px-6 pb-3 rounded ml-auto" >APPLY NOW</motion.div>
           </div>
         </motion.nav>
         </div>)}
