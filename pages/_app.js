c40b1d8953ee0c87990bcf01563faa53fc4d02ee
Vinoth <vkalyaanasundaram@kapitus.com>
2022-01-21 20:02:30 +0530

    Kapitus title element added.

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ export default function App({ Component, pageProps, statusCode }) {
     <>
       <ApolloProvider client={client}>
         {/* <Analytics /> */}
-
+        <title>Kapitus</title>
         <Component {...pageProps} />
       </ApolloProvider>
     </>
